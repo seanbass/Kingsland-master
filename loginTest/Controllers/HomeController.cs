e053f1b36ee745373d7e5e2c168cc84a7b02d0b7
seanbass <seandbass@gmail.com>
2017-06-26 15:10:44 -0700

    Kingsland
    init

---
+++ loginTest/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using System.Net;
+using InventoryDash.Models;
+
+namespace InventoryDash.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Menu()
+        {
+            return View();
+        }
+    }
+}
